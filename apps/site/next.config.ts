4bb9446e970ac4272b1b6e83f2d7c98034cae5de
ahnchulwoo <cwoo1090@gmail.com>
2026-06-02 09:01:20 +0900

    fix: redirect old Younghun article slug

--- apps/site/next.config.ts
+++ apps/site/next.config.ts
@@ -2,6 +2,16 @@ import createNextIntlPlugin from 'next-intl/plugin'
 
 const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts')
 
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/blog/meetup-2-younghoon',
+        destination: '/blog/meetup-2-younghun',
+        permanent: true,
+      },
+    ]
+  },
+}
 
 export default withNextIntl(nextConfig)
